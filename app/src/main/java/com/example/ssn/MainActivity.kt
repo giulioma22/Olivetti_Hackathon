9d2c1dbda2f56cb4b306d7f1e72d668927b1e1bb
Edgar Munro <munroedgar@hotmail.com>
2019-10-05 14:20:37 +0200

    Merge branch 'master' of /home/elmuro/StudioProjects/SSN with conflicts.

--- app/src/main/java/com/example/ssn/MainActivity.kt
+++ app/src/main/java/com/example/ssn/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.ssn
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DoSomething.setOnClickListener {
-            d("olivetti","Do something!")
-            startActivity(Intent(this, AboutMe::class.java))
-        }
+
+    }
+
+
+    fun fbPost(){
+        val textView = findViewById<TextView>(R.id.text)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://graph.facebook.com/104577460953350/feed?"
+
+        val stringRequest = StringRequest(
+            Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+        queue.add(stringRequest)
     }
 }
--- app/src/main/java/com/example/ssn/Utils.java
+++ app/src/main/java/com/example/ssn/Utils.java
@@ -1,16 +1,22 @@
 package com.example.ssn;
 
-import java.io.BufferedInputStream;
+
+import android.net.Uri;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
 
 
 public class Utils {
-    static String fbKey = "EAAFa7O0P5AkBAHVDs5siDoHToBXB8cmsSUePwchB42DzlD2wCVVzADv2Hs8EsLzONIhbVAUgiOVWUrl4M0UZCmZCNAv0J5LTNfZARUZCBQeRkEP4VogaZBP4piblwloUqavIhmokz20YpKZAqvKyTU7sCWfZAfDpz9ZBuanXsAkobwIgis4i09sHzwsYMvna3XHJhqvWgeDSbQZDZD";
+    static String fbKey = "Bearer EAAFa7O0P5AkBAHVDs5siDoHToBXB8cmsSUePwchB42DzlD2wCVVzADv2Hs8EsLzONIhbVAUgiOVWUrl4M0UZCmZCNAv0J5LTNfZARUZCBQeRkEP4VogaZBP4piblwloUqavIhmokz20YpKZAqvKyTU7sCWfZAfDpz9ZBuanXsAkobwIgis4i09sHzwsYMvna3XHJhqvWgeDSbQZDZD";
 
     public static ProductBatch getProducts(String filename){
         //TODO generate or fetch from file
@@ -18,30 +24,49 @@ public class Utils {
     }
 
 
-    public static void PostFacebookRequest(String Message){
+    public static void PostFacebookRequest(String message){
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.scheme("https")
+                .authority("graph.facebook.com")
+                .appendPath("104577460953350")
+                .appendPath("feed");
+        Uri uri = uriBuilder.build();
 
+        HttpURLConnection client = null;
         try {
-            URL url = new URL("https://graph.facebook.com/104577460953350/feed");
+            URL url = new URL(uri.toString());
 
-            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("POST");
-            try {
-                urlConnection.getHeaderField("Authorization");
-                urlConnection.addRequestProperty("Authorization", fbKey);
-                urlConnection.getResponseCode();
+            client = (HttpURLConnection) url.openConnection();
+            client.setRequestMethod("POST");
+            client.setRequestProperty("Authorization",fbKey);
+            client.setRequestProperty("message", message);
+            client.setDoOutput(true);
+            client.connect();
+            System.out.println("Sending" +client.getURL());
+            System.out.println("Response" +client.getResponseCode() + client.getResponseMessage());
 
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                System.out.println(in);
 
-            } finally {
-                urlConnection.disconnect();
-            }
+            OutputStream outputPost = new BufferedOutputStream(client.getOutputStream());
+            System.out.println(outputPost.toString());
+            outputPost.flush();
+            outputPost.close();
+
+
+
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if(client != null) // Make sure the connection is not null.
+                client.disconnect();
         }
+
+
     }
+
+
+
 
 }
